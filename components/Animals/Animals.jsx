d78590275c8207bb89a9ddf90d69736c76adefe3
DiazAdriel0 <126935419+DiazAdriel0@users.noreply.github.com>
2023-06-01 10:08:06 -0300

    Add files via upload

---
+++ components/Animals/Animals.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import styledAnimals from './Animals.module.css'
+
+export default class Animals extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>
+      {this.props.animals.map((animal,index) => {
+        return (
+          <div key={index}>
+            <h5>{animal.name}</h5>
+            <img src={animal.image} alt={animal.name} width="300px"></img>
+            <br/>
+            <span>{animal.specie}</span>
+          </div>
+        )
+      })}
+    </div>
+  }
+}
